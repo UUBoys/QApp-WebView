7bf6cd1f00ff4a4d45abaf3f52764f5b917904fc
Jan Port <honzaport@seznam.cz>
2023-10-11 19:29:43 +0200

    Signin redesign

--- src/pages/auth/signin.tsx
+++ src/pages/auth/signin.tsx
@@ -46,64 +46,65 @@ export const SignIn = () => {
   };
 
   return (
-    // @ts-ignore broken definition of LoadingOverlay
-      <div className="flex h-screen w-full items-center bg-white">
-        {" "}
-        <div className="mx-auto flex h-[500px] w-11/12  flex-col gap-20 rounded-md border-[.3px] p-10 shadow-2xl sm:w-[400px] sm:gap-32">
-          <div className="w-full text-center text-3xl font-bold text-black">
-            Login
-          </div>
-          <div className="w-full ">
-            {defaultError !== "" && (
-              <p className="mt-2 text-xl italic text-red-500">{defaultError}</p>
-            )}
-            <form action="" onSubmit={handleSubmit(handleCredentialsLogin)}>
-              <div className="mb-6">
-                <label
-                  htmlFor="email"
-                  className="mb-2 block text-sm font-medium  text-gray-900 dark:text-white"
-                >
-                  <input
-                    {...register("email")}
-                    type="email"
-                    id="email"
-                    className="block w-full rounded-lg border !border-primary-100 bg-gray-50 p-2.5 text-sm text-gray-900 shadow-lg placeholder:text-gray-400 focus:border-2  focus:ring-primary-500 dark:bg-gray-700 dark:text-white dark:placeholder:text-gray-400 dark:focus:border-primary-500 dark:focus:ring-primary-500"
-                    placeholder="Email"
-                    required
-                  />
-                </label>
-              </div>
-              <div className="mb-6">
-                <label
-                  htmlFor="password"
-                  className="mb-2 block text-sm font-medium text-gray-900 dark:text-white"
-                >
-                  <input
-                    {...register("password")}
-                    type="password"
-                    id="password"
-                    className="block w-full rounded-lg border !border-primary-100 bg-gray-50 p-2.5 text-sm text-gray-900 shadow-lg placeholder:text-gray-400 focus:border-primary-500 focus:ring-primary-500  dark:bg-gray-700 dark:text-white dark:placeholder:text-gray-400 dark:focus:border-primary-500 dark:focus:ring-primary-500"
-                    required
-                    placeholder="Password"
-                  />
-                </label>
-              </div>
-              <button
-                type="submit"
-                className="w-full rounded-lg bg-secondary-900 px-5 py-2.5 text-center text-sm font-medium text-white transition-all hover:bg-primary-200 focus:outline-none focus:ring-4  "
-              >
-                Log in
-              </button>
-            </form>
-            <Link
-              href="/auth/signup"
-              className="mt-3 block text-center text-sm font-light text-gray-600 dark:text-white"
-            >
-              Register here
-            </Link>
+    <div className="flex h-screen w-full items-center bg-gray-100">
+      <div className="mx-auto flex h-[500px] w-11/12  flex-col bg-white rounded-md  p-10 shadow-2xl sm:w-[400px]">
+        <div className="border-[10px] relative border-gray-200 rounded-full mx-auto h-[200px] justify-center w-[200px] flex bg-white items-center top-[-130px]">
+          <div className="  text-center text-6xl font-bold text-primary-500">
+            Q
           </div>
         </div>
+
+        <div className="w-full ">
+          {defaultError !== "" && (
+            <p className="mt-2 text-xl italic text-red-500">{defaultError}</p>
+          )}
+          <form action="" onSubmit={handleSubmit(handleCredentialsLogin)}>
+            <div className="mb-6">
+              <label
+                htmlFor="email"
+                className="mb-2 block text-sm font-medium  text-gray-900 dark:text-white"
+              >
+                <input
+                  {...register("email")}
+                  type="email"
+                  id="email"
+                  className="block w-full rounded-lg border !border-primary-100 bg-gray-50 p-2.5 text-sm text-gray-900 shadow-lg placeholder:text-gray-400 focus:border-2  focus:ring-primary-500 dark:bg-gray-700 dark:text-white dark:placeholder:text-gray-400 dark:focus:border-primary-500 dark:focus:ring-primary-500"
+                  placeholder="E-mail"
+                  required
+                />
+              </label>
+            </div>
+            <div className="mb-6">
+              <label
+                htmlFor="password"
+                className="mb-2 block text-sm font-medium text-gray-900 dark:text-white"
+              >
+                <input
+                  {...register("password")}
+                  type="password"
+                  id="password"
+                  className="block w-full rounded-lg border !border-primary-100 bg-gray-50 p-2.5 text-sm text-gray-900 shadow-lg placeholder:text-gray-400 focus:border-primary-500 focus:ring-primary-500  dark:bg-gray-700 dark:text-white dark:placeholder:text-gray-400 dark:focus:border-primary-500 dark:focus:ring-primary-500"
+                  required
+                  placeholder="Heslo"
+                />
+              </label>
+            </div>
+            <button
+              type="submit"
+              className="w-full rounded-lg hover:bg-secondary-900 px-5 py-2.5 text-center text-sm font-medium text-white transition-all bg-primary-500 focus:outline-none focus:ring-4  "
+            >
+              Přihlásit se
+            </button>
+          </form>
+          <Link
+            href="/auth/signup"
+            className="mt-3 block text-center text-sm font-light text-gray-600 dark:text-white"
+          >
+           Registrovat se
+          </Link>
+        </div>
       </div>
+    </div>
   );
 };
 
