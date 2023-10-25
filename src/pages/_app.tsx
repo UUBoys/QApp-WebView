4c93ee14ecedb8b7b18ec58a1fa43ac9e5a8356b
Jan Port <honzaport@seznam.cz>
2023-10-25 14:54:48 +0200

    vercel fix

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -11,7 +11,7 @@ import NavBar from "@/modules/common/components/NavBar";
 import { SessionProvider } from "next-auth/react";
 
 import { I18nextProvider } from "react-i18next";
-import { i18n } from "../i18n";
+import i18n from "../i18n";
 
 const queryClient = new QueryClient({
   defaultOptions: {
