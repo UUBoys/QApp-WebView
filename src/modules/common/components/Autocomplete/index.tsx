29b8516d89e93ce1b3c616027905a92c249873ae
Daniel Seiner <51313652+dannyseiner@users.noreply.github.com>
2023-11-01 16:53:45 +0100

    autocomplete bugfix

--- src/modules/common/components/Autocomplete/index.tsx
+++ src/modules/common/components/Autocomplete/index.tsx
@@ -1,45 +1,57 @@
-import { Fragment, useEffect, useState } from 'react'
-import { Combobox, Transition } from '@headlessui/react'
+import { Fragment, useEffect, useState } from "react";
+import { Combobox, Transition } from "@headlessui/react";
 
-import CheckIcon from '@mui/icons-material/Check';
-import ExpandLessIcon from '@mui/icons-material/ExpandLess';
-import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
-
+import CheckIcon from "@mui/icons-material/Check";
+import ExpandLessIcon from "@mui/icons-material/ExpandLess";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 
 type AutocompleteProps = {
-  options: AutocompleteItemProps[]
-  defaultSelected?: AutocompleteItemProps
-  defaultInputValue?: string
-  onChange?: (e: AutocompleteItemProps | undefined) => void
-  className?: string
-}
+  options: AutocompleteItemProps[];
+  defaultSelected?: AutocompleteItemProps;
+  defaultInputValue?: string;
+  onChange?: (e: AutocompleteItemProps | undefined) => void;
+  className?: string;
+};
 
 export type AutocompleteItemProps = {
-  label: string
-  value: any
-}
+  label: string;
+  value: any;
+};
 
-export default function Autocomplete({options, defaultSelected, defaultInputValue, onChange, className = ''}: AutocompleteProps) {
-  const [selected, setSelected] = useState<AutocompleteItemProps | undefined>(defaultSelected || undefined)
-  const [query, setQuery] = useState<string>(defaultInputValue || "")
-  const [filteredItems, setFilteredItems] = useState<AutocompleteItemProps[]>(options)
+export default function Autocomplete({
+  options,
+  defaultSelected,
+  defaultInputValue,
+  onChange,
+  className = "",
+}: AutocompleteProps) {
+  const [selected, setSelected] = useState<AutocompleteItemProps | undefined>(
+    defaultSelected || undefined
+  );
+  const [query, setQuery] = useState<string>(defaultInputValue || "");
+  console.log("query", query);
+  const [filteredItems, setFilteredItems] =
+    useState<AutocompleteItemProps[]>(options);
 
   useEffect(() => {
-    if(query){
-      setFilteredItems(options.filter((item) => item.label.toLowerCase().includes(query.toLowerCase())))
-    }else{
-      setFilteredItems(options)
+    if (query) {
+      setFilteredItems(
+        options.filter((item) =>
+          item.label.toLowerCase().includes(query.toLowerCase())
+        )
+      );
+    } else {
+      setFilteredItems(options);
     }
-  }, [query])
+  }, [query]);
 
   useEffect(() => {
-    const _option = options.find((item) => item.value === selected)
-    if(_option){
-      if(onChange) onChange(_option)
+    const _option = options.find((item) => item.value === selected);
+    if (_option) {
+      if (onChange) onChange(_option);
+      setQuery(_option.label);
     }
-  }, [selected])
-
-
+  }, [selected]);
 
   return (
     <div className={`fixed top-16 w-72 ${className}`}>
@@ -48,11 +60,14 @@ export default function Autocomplete({options, defaultSelected, defaultInputValu
           <div className="relative w-full cursor-default overflow-hidden rounded-lg bg-white text-left shadow-md focus:outline-none focus-visible:ring-2 focus-visible:ring-white/75 focus-visible:ring-offset-2 focus-visible:ring-offset-teal-300 sm:text-sm">
             <Combobox.Input
               className="w-full border-none py-2 pl-3 pr-10 text-sm leading-5 text-gray-900 focus:ring-0"
-              displayValue={(item :AutocompleteItemProps) => item.label}
               onChange={(event) => setQuery(event.target.value)}
+              value={query}
             />
             <Combobox.Button className="absolute inset-y-0 right-0 flex items-center pr-2">
-              <ExpandMoreIcon className="h-5 w-5 text-gray-400" aria-hidden="true" />
+              <ExpandMoreIcon
+                className="h-5 w-5 text-gray-400"
+                aria-hidden="true"
+              />
             </Combobox.Button>
           </div>
           <Transition
@@ -62,48 +77,53 @@ export default function Autocomplete({options, defaultSelected, defaultInputValu
             leaveTo="opacity-0"
           >
             <Combobox.Options className="absolute mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black/5 focus:outline-none sm:text-sm">
-              {filteredItems.length === 0 && query !== '' ? (
+              {filteredItems.length === 0 && query !== "" ? (
                 <div className="relative cursor-default select-none py-2 px-4 text-gray-700">
                   Nothing found.
                 </div>
               ) : (
-                filteredItems.map((item :AutocompleteItemProps, index: number) => (
-                  <Combobox.Option
-                    key={index}
-                    className={({ active }) =>
-                      `relative cursor-default select-none py-2 pl-10 pr-4 ${
-                        active ? 'bg-primary text-white' : 'text-gray-900'
-                      }`
-                    }
-                    value={item.value}
-                  >
-                    {({ selected, active }) => (
-                      <>
-                        <span
-                          className={`block truncate ${
-                            selected ? 'font-medium' : 'font-normal'
-                          }`}
-                        >
-                          {item.label}
-                        </span>
-                        {selected ? (
+                filteredItems.map(
+                  (item: AutocompleteItemProps, index: number) => (
+                    <Combobox.Option
+                      key={index}
+                      className={({ active }) =>
+                        `relative cursor-default select-none py-2 pl-10 pr-4 ${
+                          active ? "bg-primary text-white" : "text-gray-900"
+                        }`
+                      }
+                      value={item.value}
+                    >
+                      {({ selected, active }) => (
+                        <>
                           <span
-                            className={`absolute inset-y-0 left-0 flex items-center pl-3 ${
-                              active ? 'text-white' : 'text-primary'
+                            className={`block truncate ${
+                              selected ? "font-medium" : "font-normal"
                             }`}
                           >
-                            <CheckIcon className="h-5 w-5" aria-hidden="true" />
+                            {item.label}
                           </span>
-                        ) : null}
-                      </>
-                    )}
-                  </Combobox.Option>
-                ))
+                          {selected ? (
+                            <span
+                              className={`absolute inset-y-0 left-0 flex items-center pl-3 ${
+                                active ? "text-white" : "text-primary"
+                              }`}
+                            >
+                              <CheckIcon
+                                className="h-5 w-5"
+                                aria-hidden="true"
+                              />
+                            </span>
+                          ) : null}
+                        </>
+                      )}
+                    </Combobox.Option>
+                  )
+                )
               )}
             </Combobox.Options>
           </Transition>
         </div>
       </Combobox>
     </div>
-  )
+  );
 }
--- src/modules/common/components/Tabs/index.tsx
+++ src/modules/common/components/Tabs/index.tsx
@@ -1,43 +1,66 @@
-import { useEffect, useState } from 'react'
-import clsx from 'clsx'
+import { useEffect, useState } from "react";
+import clsx from "clsx";
 
 type TabProps = {
-  tabs: TabItemProps[]
-  defaultSelected?: TabItemProps
-  onChange?: (e: TabItemProps | undefined) => void
-  className?: string
-}
+  tabs: TabItemProps[];
+  defaultSelected?: TabItemProps;
+  onChange?: (e: TabItemProps | undefined) => void;
+  className?: string;
+};
 
 export type TabItemProps = {
-  label?: string
-  value?: any
-  className?: string
-  icon?: React.ReactNode
-}
+  label?: string;
+  value?: any;
+  className?: string;
+  icon?: React.ReactNode;
+};
 
-export default function Tabs({ tabs, defaultSelected, onChange, className = '' }: TabProps) {
-  const [selected, setSelected] = useState<TabItemProps | undefined>(defaultSelected || undefined)
+export default function Tabs({
+  tabs,
+  defaultSelected,
+  onChange,
+  className = "",
+}: TabProps) {
+  const [selected, setSelected] = useState<TabItemProps | undefined>(
+    defaultSelected || undefined
+  );
 
   useEffect(() => {
-    if(onChange) onChange(selected)
-  }, [selected])
+    if (onChange) onChange(selected);
+  }, [selected]);
 
   const handelTabChange = (e: TabItemProps) => {
-    if(e !== selected) {
-      setSelected(e)
-      if(onChange) onChange(e)
+    if (e !== selected) {
+      setSelected(e);
+      if (onChange) onChange(e);
     }
-  }
+  };
 
   return (
-    <div className={clsx("w-fit flex items-center justify-center bg-white shadow-2xl p-3 rounded-[14px] gap-[10px]", className)}>
+    <div
+      className={clsx(
+        "w-fit flex items-center justify-center bg-white shadow-2xl p-3 rounded-[14px] gap-[10px]",
+        className
+      )}
+    >
       {tabs.map((tab, index) => (
-        <div className={clsx("flex flex-col justify-center items-center bg-white p-3 transition-all cursor-pointer rounded-[12px] h-full", tab.className, selected === tab ? "bg-primary-100  text-primary" : "text-black")} key={index} onClick={() => handelTabChange(tab)}>
-            {tab.icon}
-            {tab.label && <p className={clsx("font-medium  text-[12px]")}>{tab.label}</p>}
+        <div
+          className={clsx(
+            "flex flex-col justify-center items-center bg-white p-3 transition-all cursor-pointer rounded-[12px] h-full",
+            tab.className,
+            selected === tab
+              ? "bg-[rgba(255,173,50,0.3)]  text-primary"
+              : "text-black"
+          )}
+          key={index}
+          onClick={() => handelTabChange(tab)}
+        >
+          {tab.icon}
+          {tab.label && (
+            <p className={clsx("font-medium  text-[12px]")}>{tab.label}</p>
+          )}
         </div>
       ))}
-
     </div>
-  )
+  );
 }
